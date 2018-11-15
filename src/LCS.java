9dae96c725fba632aff637785c4989ecacde1979
NicolaiV <nicolai.verbaarschot@gmail.com>
2018-11-15 11:42:29 +0100

    LCS tested and working

---
+++ src/LCS.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class LCS {
+	// Should print the longest common subsequence of a and b
+	public void lcs(String a, String b) {
+		// Implement this
+	}
+	
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		
+		new LCS().lcs(input.readLine(), input.readLine());
+	}
+}
